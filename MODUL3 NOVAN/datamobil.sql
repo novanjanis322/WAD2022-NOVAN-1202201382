86048873fec0f14f53fa8c0333ddbdd03fa5f314
novanjanis322 <novangfc@gmail.com>
2022-11-20 21:43:54 +0700

    kurang sql filenya

---
+++ MODUL3 NOVAN/datamobil.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 20, 2022 at 03:42 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_wad_modul3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `datamobil`
+--
+
+CREATE TABLE `datamobil` (
+  `id_mobil` int(255) NOT NULL,
+  `nama_mobil` varchar(255) NOT NULL,
+  `pemilik_mobil` varchar(255) NOT NULL,
+  `merk_mobil` varchar(255) NOT NULL,
+  `tanggal_beli` date NOT NULL,
+  `deskripsi` text NOT NULL,
+  `foto_mobil` varchar(255) NOT NULL,
+  `status_pembayaran` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `datamobil`
+--
+
+INSERT INTO `datamobil` (`id_mobil`, `nama_mobil`, `pemilik_mobil`, `merk_mobil`, `tanggal_beli`, `deskripsi`, `foto_mobil`, `status_pembayaran`) VALUES
+(7, 'mobil merah', 'Novan - 1202201382', 'Super Car', '2022-11-20', 'asdadqwe', 'images_gta-san-andreas_vehicles_super-gt.webp', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `datamobil`
+--
+ALTER TABLE `datamobil`
+  ADD PRIMARY KEY (`id_mobil`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `datamobil`
+--
+ALTER TABLE `datamobil`
+  MODIFY `id_mobil` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
