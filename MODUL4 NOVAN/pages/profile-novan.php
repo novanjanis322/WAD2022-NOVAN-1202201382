45c974e473429f3c09b65c53f4154b164be167dc
novanjanis322 <novangfc@gmail.com>
2022-11-27 12:47:51 +0700

    modul4

--- MODUL4 NOVAN/pages/profile-novan.php
+++ MODUL4 NOVAN/pages/profile-novan.php
@@ -18,21 +18,43 @@
                 <div class="mb-3 row">
                     <label for="displayemail" class="col-sm-2 col-form-label">email<a style="color:red">*</a></label>
                     <div class="col-sm-10">
-                        <input type="text" readonly class="form-control-plaintext" name="displayemail" id="displayemail" value="<?php echo $_COOKIE['email']; ?>">
+                        <input type="text" class="form-control-plaintext" name="displayemail" id="displayemail" value="<?php echo $_COOKIE['email']; ?>">
                     </div>
                 </div>
                 <div class="mb-3 row">
                     <label for="displaynama" class="col-sm-2 col-form-label">nama<a style="color:red">*</a></label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" name="displaynama" id="displaynama" value="<?php echo $_COOKIE['nama']; ?>" >
+                        <input type="text" class="form-control" name="displaynama" id="displaynama" value="<?php echo $_COOKIE['nama']; ?>">
                     </div>
                 </div>
                 <div class="mb-3 row">
                     <label for="displaynomorhp" class="col-sm-2 col-form-label">nomor hp<a style="color:red">*</a></label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" name="displaynomorhp" id="displaynomorhp" value="<?php echo $_COOKIE['no_hp']; ?>" >
+                        <input type="text" class="form-control" name="displaynomorhp" id="displaynomorhp" value="<?php echo $_COOKIE['no_hp']; ?>">
                     </div>
                 </div>
+                <hr>
+                <div class="mb-3 row">
+                    <label for="displaypassword" class="col-sm-2 col-form-label">password<a style="color:red">*</a></label>
+                    <div class="col-sm-10">
+                        <input type="passwrd" class="form-control" name="displaypassword" id="displaypassword" placeholder="Kata Sandi">
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="displayconfirmpassword" class="col-sm-2 col-form-label">konfirmasi password<a style="color:red">*</a></label>
+                    <div class="col-sm-10">
+                        <input type="passwrd" class="form-control" name="displayconfirmpassword" id="displayconfirmpassword" placeholder="Konfirmasi Kata Sandi">
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="displaynavbar" class="col-sm-2 col-form-label">Navbar</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="displaynavbar" id="displaynavbar" value="blue" readonly>
+                    </div>
+                </div>
+                <center>
+                    <input class="btn btn-primary booking-cta" name="edit" type="submit" value="Update" style="--bs-btn-padding-y: 10px; --bs-btn-padding-x: 30px">
+                </center>
             </div>
         </center>
     </section>
